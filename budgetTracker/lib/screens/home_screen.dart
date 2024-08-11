271391e5a27cd920174d84ed3d00fb820bb55325
AMN <87907130+AbrahamCEO@users.noreply.github.com>
2024-08-11 21:05:07 +0200

    home_screen.dart

--- budgetTracker/lib/screens/home_screen.dart
+++ budgetTracker/lib/screens/home_screen.dart
@@ -5,7 +5,7 @@ import 'package:budgetmap/widgets/transactions_cards.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-ssssssssssssssssssssssssss
+
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
